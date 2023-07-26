041e4c98238c7939edebe48fd588d9e519b34cd3
doesui <islee511@gmail>
2023-07-26 09:35:38 +0900

    연습1

--- BankAccount.py
+++ BankAccount.py
@@ -5,19 +5,19 @@ class BankAccount:
     def __init__(self, id, name, balance):
         self.__id = id      # __를 추가하면 외부에서 접근을 못함
         self.__name = name 
-        self.__balance = balance 
+        self._balance = balance 
     def deposit(self, amount):
-        self.__balance += amount 
+        self._balance += amount 
     def withdraw(self, amount):
-        self.__balance -= amount
+        self._balance -= amount
     def __str__(self):
         return "{0} , {1} , {2}".format(self.__id, \
-            self.__name, self.__balance)
+            self.__name, self._balance)
 
 #인스턴스 객체를 생성
 account1 = BankAccount(100, "전우치", 15000)
 account1.withdraw(3000)
 print(account1)
 
-# print(account1.__balance)
-print(globals())
+print("외부에서 접근한 잔액 확인:", account1._balance)  # 단일 밑줄 사용하면 접근은 가능함
+# print(globals())
--- 상속01.py
+++ 상속01.py
@@ -2,21 +2,40 @@ class Person:
     def __init__(self, name, phoneNumber):
         self.name = name
         self.phoneNumber = phoneNumber
-
     def printInfo(self):
-        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name,
+             self.phoneNumber)) 
+    def working(self):
+        print("현재 작업중")
+    def coding(self):
+        print("현재 코딩중")
 
-class Student(Person):
+
+class Student(Person):  # Person을 상속한다
+    # 상속받고 덥어쓰기(재정의, override)
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        Person.__init__(self, name, phoneNumber)
+        # self.name = name
+        # self.phoneNumber = phoneNumber
         self.subject = subject
         self.studentID = studentID
+    # def printInfo(self):
+    #     return super().printInfo()
+    # 재정의
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name,
+             self.phoneNumber))
+        print("Info(subject:{0}, studentID: {1})".format(self.subject,
+             self.studentID))
 
-
+# 인스턴스 생성
 p = Person("전우치", "010-222-1234")
 s = Student("이순신", "010-111-1234", "컴공", "991122")
-print(p.__dict__)
-print(s.__dict__)
+# print(p.__dict__)
+# print(s.__dict__)
 
+p.printInfo()
+s.printInfo()
+s.working()
+s.coding()
 
